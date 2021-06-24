7a00dfad7567d765bc5c8228da00a64fea73a2b4
Matt Landis <matt@windmill.engineering>
2021-06-24 10:20:24 -0400

    fix build

--- src/extensionVariables.ts
+++ src/extensionVariables.ts
@@ -1,7 +1,6 @@
 import { KubeConfig } from "@kubernetes/client-node";
 import { ExtensionContext } from "vscode";
 import { TiltDevV1alpha1Api } from "./gen/api";
-import { StatusBar } from "./statusBar";
 
 export namespace ext {
     export let context: ExtensionContext;
@@ -9,6 +8,4 @@ export namespace ext {
     export let config: KubeConfig;
 
     export let client: TiltDevV1alpha1Api;
-
-    export let statusBar: StatusBar;
 }
